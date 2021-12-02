92af13e9ccaeca5a051e23470a48feb56639e205
Rakesh <rakeshbhaker10@gmail.com>
2021-12-02 22:26:39 +0530

    #2 header file is updated.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+{/* // When the user scrolls down 50px from the top of the document, resize the header's font size */}
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("headerLogo").style.fontSize = "x-large";
+    document.getElementById("headerLogo").style.fontWeight = "500";
+  } else {
+    document.getElementById("headerLogo").style.fontSize = "xx-large";
+    document.getElementById("headerLogo").style.fontWeight = "bolder";
+  }
+}
